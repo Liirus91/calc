cb99978ffded8d31825469326f7d3b546a3495e1
Liirus91 <61900756+Liirus91@users.noreply.github.com>
2020-03-17 13:36:02 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# calc
+Created by Codrops
+
+Please read more about our license here: http://tympanus.net/codrops/licensing/ 
---
+++ js/arithmetic.js
@@ -0,0 +1,12 @@
+function arithm() {
+	var a = parseInt(document.getElementById('a').value);
+	var b = parseInt(document.getElementById('b').value);
+
+	if (isNaN(a)==true) a=0;
+	if (isNaN(b)==true) b=0;
+
+	var c = a + b;
+
+	document.getElementById('result').innerHTML = a + " + " + b + " = " + c;
+
+}
